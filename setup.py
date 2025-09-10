19039c03692629ddec110214a84ba68a9178ccb9
Thanay Krishnareddypeta <kri5hnareddyp3ta@gmail.com>
2025-09-10 13:16:52 -0400

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="price_engine",
+    version="2.0.0",
+    author="Your Name",
+    description="An advanced, end-to-end engine for price elasticity modeling and profit optimization.",
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    packages=find_packages(),
+    install_requires=[
+        "numpy",
+        "pandas",
+        "scikit-learn",
+        "statsmodels",
+        "xgboost",
+        "pygam"
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.8',
+)
